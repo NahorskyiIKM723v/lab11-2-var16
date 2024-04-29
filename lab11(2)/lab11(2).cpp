f59a26a65e750393d0357379ffc58c9c5683eaa6
igor <igor@DESKTOP-EH4IM96>
2024-04-30 00:01:20 +0300

    Добавьте файлы проекта.

---
+++ lab11(2)/lab11(2).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int* pokaschik1, * pokaschik2;
+
+    pokaschik1 = new int;
+    pokaschik2 = new int;
+
+    *pokaschik1 = 22;
+    *pokaschik2 = 33;
+
+    cout << "Початкові значення:\n";
+    cout << "Покажчик 1 = " << *pokaschik1 << endl;
+    cout << "Покажчик 2 = " << *pokaschik2 << endl;
+
+    int temp = *pokaschik1;
+    *pokaschik1 = *pokaschik2;
+    *pokaschik2 = temp;
+
+    cout << "Результат обміну:\n";
+    cout << "Покажчик 1 = " << *pokaschik1 << endl;
+    cout << "Покажчик 2 = " << *pokaschik2 << endl;
+
+    delete pokaschik1;
+    delete pokaschik2;
+
+    return 0;
+}
